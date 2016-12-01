3888595a9ffbe77a3dcd4ca65ed804edb0dd93d7
Kris Szafranski <kdszafranski@gmail.com>
2016-11-30 21:23:07 -0600

    rolled server to lecture start

--- server/app.js
+++ server/app.js
@@ -2,32 +2,17 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-var mongoose = require('mongoose');
 var index = require('./routes/index');
-var people = require('./routes/people.js');
 
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(express.static(path.join(__dirname, './public')));
-// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // needed for angular requests
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/person', people);
 app.use('/', index);
 
 /** ---------- MONGOOSE CONNECTION HANDLING ---------- **/
-var databaseUri = 'mongodb://localhost:27017/sigma';
-
-mongoose.connect(databaseUri);
-
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose connected to ', databaseUri);
-});
-
-mongoose.connection.on('error', function (err) {
-  console.log('Mongoose failed to connect because error: ', err);
-});
 
 
 /** ---------- START SERVER ---------- **/
