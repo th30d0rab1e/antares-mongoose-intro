b6157ca72b34454d9b75b37a1bbf5bfd80dd0aab
Kris Szafranski <kdszafranski@gmail.com>
2017-07-20 13:18:29 -0500

    rolled back to beginning

--- server/public/scripts/controllers/people.controller.js
+++ server/public/scripts/controllers/people.controller.js
@@ -1,63 +1,7 @@
 myApp.controller('PeopleController', ['$http', function($http) {
-    var self = this;
-    self.people = [];
-    self.newPerson = {};
+    var vm = this;
+    vm.newPerson = {};
 
-    // Start app
-    getData();
 
-    // Get all people
-    function getData() {
-      $http.get('/person')
-      .then(function(response) {
-        self.people = response.data;
-      },
-      function(response) {
-        console.log('get error:', response);
-      })
-      ;
-    }
-
-    // add person
-    self.addPerson = function() {
-      console.log('add person');
-      $http.post('/person', self.newPerson)
-        .then(function(response) {
-          // getData();
-          self.people.push(response.data);
-          console.log(response);
-        },
-        function(response) {
-          console.log('post error:', response);
-        }
-      );
-    };
-
-    // delete person
-    self.deletePerson = function(id) {
-      console.log('delete person');
-      $http.delete('/person/' + id)
-        .then(function(response) {
-          getData();
-        },
-        function(response) {
-          console.log('delete error:', response);
-        }
-      );
-    }
-
-    // update person
-    self.updatePerson = function(id) {
-      console.log('update person');
-      var data = {location: 'NOT Minneapolis'};
-      $http.put('/person/' + id, data)
-        .then(function(response) {
-          getData();
-        },
-        function(response) {
-          console.log('update error:', response);
-        }
-      );
-    }
 
 }]);
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var index = require('./routes/index');
-var people = require('./routes/people');
-var mongoose = require('mongoose');
 
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -12,20 +10,8 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(bodyParser.json()); // needed for angular requests
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/person', people);
 app.use('/', index);
 
-/** ---------- MONGOOSE CONNECTION HANDLING ---------- **/
-var databaseUri = 'mongodb://localhost:27017/sigma';
-mongoose.connect(databaseUri);
-
-mongoose.connection.on('connected', function() {
-  console.log('mongoose connected to ', databaseUri);
-});
-
-mongoose.connection.on('error', function(err) {
-  console.log('mongoose connection error: ', err);
-});
 
 /** ---------- START SERVER ---------- **/
 app.set('port', process.env.PORT || 5000);
